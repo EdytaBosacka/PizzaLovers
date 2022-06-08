1e9b853404ec3b7161866f9e8a1f736541ad2385
Edyta <edyta.bosacka@gmail.com>
2022-06-08 22:28:29 +0200

    Implementing drag events on dropzone

--- client/src/components/ImageDropzone.tsx
+++ client/src/components/ImageDropzone.tsx
@@ -1,17 +1,50 @@
 import CloudUploadOutlinedIcon from '@mui/icons-material/CloudUploadOutlined';
-import { useRef } from 'react';
+import AddPhotoAlternateOutlinedIcon from '@mui/icons-material/AddPhotoAlternateOutlined';
+import { useRef, useState } from 'react';
 import './ImageDropzone.css';
 
-function ImageDropzone () {
+function ImageDropzone(props: any) {
 
+    const [highlight, setHighlight] = useState(false);
     const fileInputRef = useRef<HTMLInputElement>(null);
+
     const openFileDialog = () => {
         fileInputRef.current?.click();
     }
+
+    const onImageFilesAdded = (e: Event) => {
+        const images = (e.target as HTMLInputElement).files;
+        if (props.onImageFilesAdded) {
+            const imageArray = Array.from(images || []);
+            props.onImageFilesAdded(imageArray);
+        }
+    }
+    const onDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        setHighlight(true);
+    }
+    const onDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+        setHighlight(false);
+    }
+    const onDrop = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        const images = e.dataTransfer.files;
+        if (props.onImageFilesAdded) {
+            const imageArray = Array.from(images || []);
+            props.onImageFilesAdded(imageArray);
+        }
+        setHighlight(false);
+    }
+
     return (
-        <div className="ImageDropzone" onClick={openFileDialog}>
-            <CloudUploadOutlinedIcon sx={{color:"rgb(187, 186, 186)", fontSize: "48px",  opacity: "0.7"}}/>
-            <input ref={fileInputRef} className="FileInput" type="file"/>
+        <div className={`ImageDropzone ${highlight ? 'Highlight' : ''}`}
+             onClick={openFileDialog}
+             onDragOver={onDragOver} 
+             onDragLeave={onDragLeave}
+             onDrop={onDrop}
+        >
+            <AddPhotoAlternateOutlinedIcon sx={{ color: "rgb(187, 186, 186)", fontSize: "48px", opacity: "0.7" }} />
+            <input ref={fileInputRef} className="FileInput" type="file" />
         </div>
 
     )
